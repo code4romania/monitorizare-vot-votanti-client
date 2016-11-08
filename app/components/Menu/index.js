18e77d7cc9c0f509f51829926da302b754fbad86
ccioloca <ccioloca@pentalog.fr>
2016-11-08 11:50:51 +0200

    Add menu link for sesizari

--- app/components/Menu/index.js
+++ app/components/Menu/index.js
@@ -17,6 +17,7 @@ export default class Menu extends React.PureComponent {
       <MenuList>
         <MenuItem id="acasa"><StyledLink to="acasa">ADAUGA SESIZARE/ACASA</StyledLink></MenuItem>
         <MenuItem id="statistici"><StyledLink to="statistici">LIVE STATS</StyledLink></MenuItem>
+        <MenuItem id="sesizari"><StyledLink to="sesizari">SESIZARI</StyledLink></MenuItem>
         <MenuItem id="reguli-vot"><StyledLink to="reguli-vot">REGULI VOT</StyledLink></MenuItem>
         <MenuItem id="despre-noi"><StyledLink to="despre-noi">DESPRE NOI</StyledLink></MenuItem>
       </MenuList>
