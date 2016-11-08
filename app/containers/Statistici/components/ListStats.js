236d3ea1fafa6e26e8f71b10a12c2fc6183dc25a
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-11-08 14:11:12 +0200

    Live stats list: map items directly in JSX instead of using a variable

--- app/containers/Statistici/components/ListStats.js
+++ app/containers/Statistici/components/ListStats.js
@@ -3,13 +3,11 @@ import StatsItem from './StatsItem';
 
 export default class ListStats extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const statsItems = this.props.stats.map((item, index) =>
-      <StatsItem key={index} title={item.title} subtitle={item.subtitle} />
-    );
-
     return (
       <div className="row">
-        {statsItems}
+        {this.props.stats.map((item, index) =>
+          <StatsItem key={index} title={item.title} subtitle={item.subtitle} />
+        )}
       </div>
     );
   }
