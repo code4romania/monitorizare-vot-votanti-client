2c9d060e6de58a71819720e2740c5e044c334868
ccioloca <ccioloca@pentalog.fr>
2016-11-09 12:47:12 +0200

    Add basic text to About us page

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 const styles = {
   textAlign: 'center',
   marginTop: '10px',
+  borderTop: '1px solid lightgray',
+  paddingTop: '15px',
 };
 
 const removeHrefStyle = {
@@ -14,7 +16,7 @@ export default function Footer() {
   return (
     <div style={styles}>
       <div>Email: <a href="mailto:youremailaddress" style={removeHrefStyle}>test@test.com</a></div>
-      <h5>Monitorizare vot @2016</h5>
+      <h5 style={{ margin: '5px' }}>Monitorizare vot @2016</h5>
     </div>
   );
 }
--- app/containers/DespreNoi/messages.js
+++
@@ -1,8 +0,0 @@
-import { defineMessages } from 'react-intl';
-
-export default defineMessages({
-  header: {
-    id: 'app.components.DespreNoi.header',
-    defaultMessage: 'Despre noi!',
-  },
-});
