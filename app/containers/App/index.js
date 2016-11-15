5943533cd1902bbdf93dcac8911d32f64e02cd03
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-11-15 14:59:59 +0200

    Revert "Full-width app container"
    This reverts commit 1a103d3028a33ab13fbccf93081b383af87fb85f.

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import sassStyles from './styles.scss';
 
 export default class App extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
@@ -11,7 +10,7 @@ export default class App extends React.PureComponent { // eslint-disable-line re
 
   render() {
     return (
-      <div className={sassStyles.container}>
+      <div className="col-md-10 col-md-offset-1">
         <Header {...this.props.location} />
         {React.Children.toArray(this.props.children)}
         <Footer />
