a3578113e1b06055c6536c4de653d07251e3ef68
Alin Chiuaru <alin.chiuaru@gmail.com>
2016-11-28 22:18:34 +0200

    Stats Item - adjust font sizes and paddings

--- app/containers/Statistici/components/StatsItem/index.js
+++ app/containers/Statistici/components/StatsItem/index.js
@@ -3,18 +3,22 @@ import styled from 'styled-components';
 
 // @TODO: Move each of the following styled components into separate files.
 const Title = styled.h1`
-  font-size: 48px;
+  font-size: 40px;
   font-weight: 900;
   margin: 0;
   padding: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const Subtitle = styled.h2`
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 500;
   opacity: 0.7;
   margin: 0;
   padding: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const TextWrapper = styled.div`
@@ -24,17 +28,19 @@ const TextWrapper = styled.div`
 const RowWrapper = styled.div`
   background: #FFCC00;
   color: #5F288D;
-  padding: 35px;
+  padding: 25px;
+  height: 100%;
 `;
 
 const Wrapper = styled.div`
-  padding: 15px;
+  padding: 10px 20px;
   box-sizing: border-box;
+
   &:nth-child(-n+2) > .row-wrap {
     background: #5F288D;
     color: #FFF;
     > .text-wrap > .item-title {
-      font-size: 72px;
+      font-size: 50px;
     }
   }
 `;
