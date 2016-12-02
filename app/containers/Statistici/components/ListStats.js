80f48c193f4f314489ceef5faef1b80ec3351ef9
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-02 16:53:45 +0100

    added rules icons

--- app/containers/Statistici/components/ListStats.js
+++ app/containers/Statistici/components/ListStats.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import LeftColumn from './leftColumn';
 import RightColumn from './rightColumn';
 
+const LastUpdate = styled.p`
+  font-size: 14px;
+  color: #999;
+  font-weight: 400;
+  text-align: center;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
 
 export default function ListStats(props) {
   return (
     <div className="row">
+      <LastUpdate className="col-xs-12">
+        Ultima actualizare a fos la 15:34:30 11 Decembrie 2016
+      </LastUpdate>
       <div className="col-xs-12 col-md-6">
         <LeftColumn {...props} />
       </div>
--- app/containers/Statistici/components/StatsInfo.js
+++ app/containers/Statistici/components/StatsInfo.js
@@ -1,11 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const LastUpdate = styled.p`
-  font-size: 12px;
-  color: #999;
-  font-weight: 400;
-`;
 
 export default function StatsInfo() {
   return (
@@ -13,7 +6,6 @@ export default function StatsInfo() {
       <div className="col-xs-12 col-md-offset-2 col-md-8 page-hero">
         <h1>Statistici in timp real</h1>
         <p>Astonishment with pretty stories for which theres little good evidence, Cambrian explosion. Shores of the cosmic ocean Euclid, at the edge of forever intelligent beings, tingling of the spine Drake Equation vanquish the impossible, with pretty stories for which theres little good evidence descended.</p>
-        <LastUpdate>Ultima actualizare a fos la 15:34:30 11 Decembrie 2016</LastUpdate>
       </div>
     </div>
   );
