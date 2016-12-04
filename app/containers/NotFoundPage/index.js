8a0b9539c61d3953d870d2cad4fbfe04b8ab6554
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-04 21:54:42 +0100

    404 page

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 import messages from './messages';
+
+const Heading = styled.h1`
+  text-align: center;
+  margin: 100px 0;
+`;
 
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <section className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <Heading>
+              <FormattedMessage {...messages.header} />
+            </Heading>
+          </div>
+        </div>
+      </section>
     );
   }
 }
