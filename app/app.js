3aa5a890c30e46917970ef9693bc42450e223f39
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-06 23:10:21 +0100

    stable. no MuiThemeProvider

--- app/app.js
+++ app/app.js
@@ -22,13 +22,11 @@ import { Provider } from 'react-redux';
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
-import injectTapEventPlugin from 'react-tap-event-plugin';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 
 import configureStore from './store';
 
 import LanguageProvider from 'containers/LanguageProvider';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 injectTapEventPlugin();
 
@@ -60,49 +58,6 @@ const rootRoute = {
   component: App,
   childRoutes: createRoutes(store),
 };
-
-// const mvTheme = getMuiTheme({
-//   fontFamily: '"Open Sans", sans-serif',
-
-//   palette: {
-//     textColor: '#2d2d2d',
-//   },
-
-//   textField: {
-//     textColor: 'rgba(95, 40, 141, 1)',
-//     hintColor: 'rgba(95, 40, 141, 0.6)',
-//     floatingLabelColor: 'rgba(95, 40, 141, 0.6)',
-//     disabledTextColor: 'rgba(95, 40, 141, 0.6)',
-//     errorColor: 'rgba(200, 0, 0, 1)',
-//     focusColor: 'rgba(95, 40, 141, 1)',
-//     borderColor: 'rgba(95, 40, 141, 0.4)',
-//   },
-
-//   raisedButton: {
-//     color: 'rgba(95, 40, 141, 1)',
-//     textColor: 'rgba(255, 255, 255, 1)',
-//     primaryColor: 'rgba(95, 40, 141, 1)',
-//     primaryTextColor: 'rgba(255, 255, 255, 1)',
-//     secondaryColor: 'rgba(255, 204, 0, 1)',
-//     secondaryTextColor: 'rgba(95, 40, 141, 1)',
-//     disabledColor: 'rgba(95, 40, 141, 0.1)',
-//     disabledTextColor: 'rgba(95, 40, 141, 1)',
-//     fontSize: '14px',
-//   },
-
-//   toggle: {
-//     thumbOnColor: 'rgba(95, 40, 141, 1)',
-//     thumbOffColor: 'rgba(255, 255, 255, 1)',
-//     thumbRequiredColor: 'rgba(95, 40, 141, 1)',
-//     trackOnColor: 'rgba(159, 106, 85, 1)',
-//     labelColor: 'rgba(95, 40, 141, 0.6)',
-//   },
-
-//   menuItem: {
-//     selectedTextColor: 'rgba(95, 40, 141, 1)',
-//   },
-
-// });
 
 const render = (translatedMessages) => {
   ReactDOM.render(
---
+++ app/mvTheme.js
@@ -0,0 +1,41 @@
+export default {
+  fontFamily: '"Open Sans", sans-serif',
+
+  palette: {
+    textColor: '#2d2d2d',
+  },
+
+  textField: {
+    textColor: 'rgba(95, 40, 141, 1)',
+    hintColor: 'rgba(95, 40, 141, 0.6)',
+    floatingLabelColor: 'rgba(95, 40, 141, 0.6)',
+    disabledTextColor: 'rgba(95, 40, 141, 0.6)',
+    errorColor: 'rgba(200, 0, 0, 1)',
+    focusColor: 'rgba(95, 40, 141, 1)',
+    borderColor: 'rgba(95, 40, 141, 0.4)',
+  },
+
+  raisedButton: {
+    color: 'rgba(95, 40, 141, 1)',
+    textColor: 'rgba(255, 255, 255, 1)',
+    primaryColor: 'rgba(95, 40, 141, 1)',
+    primaryTextColor: 'rgba(255, 255, 255, 1)',
+    secondaryColor: 'rgba(255, 204, 0, 1)',
+    secondaryTextColor: 'rgba(95, 40, 141, 1)',
+    disabledColor: 'rgba(95, 40, 141, 0.1)',
+    disabledTextColor: 'rgba(95, 40, 141, 1)',
+    fontSize: '14px',
+  },
+
+  toggle: {
+    thumbOnColor: 'rgba(95, 40, 141, 1)',
+    thumbOffColor: 'rgba(255, 255, 255, 1)',
+    thumbRequiredColor: 'rgba(95, 40, 141, 1)',
+    trackOnColor: 'rgba(159, 106, 85, 1)',
+    labelColor: 'rgba(95, 40, 141, 0.6)',
+  },
+
+  menuItem: {
+    selectedTextColor: 'rgba(95, 40, 141, 1)',
+  },
+};
