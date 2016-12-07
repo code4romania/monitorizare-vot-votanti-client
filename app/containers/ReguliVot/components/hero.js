3b12e494893c531ac430c8424c727cd2df00645f
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-07 12:28:50 +0100

    urzeala regulilor nu e ok pt comunicare

--- app/containers/ReguliVot/components/hero.js
+++ app/containers/ReguliVot/components/hero.js
@@ -6,7 +6,7 @@ function Hero() {
       <div className="row">
         <div className="col-xs-12 col-md-offset-2 col-md-8 page-hero">
           <h1>
-            Urzeala Regulilor
+            Cum fac o sesizare?
           </h1>
           <p>
             Vrei să trimiți o sesizare, dar nu ești sigur dacă s-a încălcat o regulă? Verifică mai jos, în ghidul alegătorului, care sunt regulile de vot, pentru a transmite informații corecte și utile.<br />Pentru a ne asigura ca sesizările privesc exact subiectul alegerilor, acestea vor fi postate pe site după o verificare făcută de echipa proiectului.
