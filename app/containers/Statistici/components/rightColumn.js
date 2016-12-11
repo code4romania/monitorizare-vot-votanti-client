040bd020781a5984c196b13f9b62778cbb97df9d
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-11 05:26:09 +0100

    removed obs stas

--- app/containers/Statistici/components/rightColumn.js
+++ app/containers/Statistici/components/rightColumn.js
@@ -4,7 +4,6 @@ import ItemStats from './item-stats';
 export default function RightColumn(props) {
   return (
     <div>
-      <h1><strong>1234</strong> sesizari de la observatori</h1>
       <h2>Statistici dupa tipurile de sesizari</h2>
       <div className="row">
         {props.stats.incidentsByType.map((item, index) =>
