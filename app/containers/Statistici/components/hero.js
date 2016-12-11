f19f1d88144305757b48cc7abbe17a1e487f6c26
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-11 09:32:19 +0100

    copy hero

--- app/containers/Statistici/components/hero.js
+++ app/containers/Statistici/components/hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="row">
       <div className="col-xs-12 col-md-offset-2 col-md-8 page-hero">
-        <h1>Statistici in timp real</h1>
+        <h1>Statistici</h1>
         <p>Monitorizare Vot îți prezintă ce se întâmplă în secțiile de votare din orașele în care se află observatori independenți, dar și ce spun cetățenii în ziua votului. Aici vei afla care sunt județele fruntașe când vine vorba de nereguli, dar și care sunt cele în care procesul electoral se desfășoară corect și fără incidente.</p>
       </div>
     </div>
