82dbfb8e13c83088acd9ce499e9c0e0ed4e1f629
Razvan Pavel <razvan.e.pavel@gmail.com>
2016-12-12 11:11:07 +0100

    copy change

--- app/containers/Acasa/MainContainer/components/LeftContainer/index.js
+++ app/containers/Acasa/MainContainer/components/LeftContainer/index.js
@@ -207,7 +207,8 @@ export class LeftContainer extends React.PureComponent {
         <Interact className="interact">
           <Sticky isActive={this.shouldBeSticky()}>
             <h2>Adaugă o sesizare</h2>
-            <p>Mai jos găsești formularul prin care poți să trimiți o sesizare. Dacă nu ești sigur ce poate fi subiectul unei sesizări, verifică aici <Link to="reguli-vot">Regulile votului</Link>. <br /> <small style={{ fontWeight: '700' }}>Toate câmpurile sunt obligatorii.</small></p>
+            <p>Mai jos găsești formularul prin care poți să trimiți o sesizare. Dacă nu ești sigur ce poate fi subiectul unei sesizări, verifică aici <Link to="reguli-vot">Regulile votului</Link>. <small><span style={{ fontWeight: 700 }}>Important:</span> sesizările trimise vor fi promovate în scop informativ în social media și către mass-media. Dacă dorești un răspuns direct de la autorități, te rugăm să te adresezi Biroului Electoral de Circumscripție sau Biroului Electoral Central. Mai multe detalii <Link to="despre-noi">aici.</Link></small></p>
+
             <AddIncident className="row interact-form add-incident">
               <div className="col-xs-12">
                 <TextField
